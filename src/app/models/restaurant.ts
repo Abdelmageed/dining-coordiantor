3b73cacf916543e757449cbcddf12278848595be
Abdelmageed <abdelmageedsayed@hotmail.com>
2018-01-17 21:04:58 +0200

    Use typegoose for models.

--- src/app/models/restaurant.ts
+++ src/app/models/restaurant.ts
@@ -1,6 +1,16 @@
-export interface Restaurant {
-    id: number,
+import { prop, arrayProp, Typegoose } from 'typegoose';
+
+// export class  Restaurant extends Typegoose {
+export class  Restaurant {    
+    @prop({ required: true })
+    id: number;
+
+    @prop()
     name: string;
+
+    @prop()
     location: string;
+
+    @arrayProp({items: Number})
     going: number[];
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,8 +1,23 @@
-export interface User {
+import { prop, arrayProp, Typegoose } from 'typegoose';
+
+// export class User extends Typegoose {
+    export class User {
+
+    @prop({ required: true })
     id: number;
+
+    @prop()
     name: string;
+
+    @prop({ required: true })
     email: string;
+
+    @prop()
     password: string;
+
+    @prop()
     token: string;
+
+    @prop()
     searchQuery: string;
 }
